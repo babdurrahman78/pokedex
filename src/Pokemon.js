195c3c9d5379ced32064adb3a4ece594de5163db
babdurrahman78 <babdurrahman78@gmail.com>
2021-07-03 15:33:39 +0700

    add styling pokemon page

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,27 +1,53 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import "./Pokemon.css";
 
 const Pokemon = () => {
-    const [pokemon, setPokemon] = useState([]);
-    const { name } = useParams();
+  const [pokemon, setPokemon] = useState([]);
+  const { name } = useParams();
 
-    useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        .then(response => response.json())
-        .then(data => setPokemon([data]));
-        console.log(pokemon);
-    }, [])
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then((response) => response.json())
+      .then((data) => setPokemon([data]));
+    console.log(pokemon);
+  }, []);
 
-    return ( 
+  return (
+    <div className="pokemon-details">
+      {pokemon.map((poke, index) => (
         <div>
-            {pokemon.map(poke => <p>{poke.weight}</p>)}
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+              index + 1
+            }.png`}
+            alt={poke.name}
+          />
+          <p>
+            {poke.name}
+            <span>{`#${index + 1}`}</span>
+          </p>
+          <div className="types">
+            {poke.types.map((type, index) => (
+              <p key={index} className="type">
+                {type.type.name}
+              </p>
+            ))}
+          </div>
+          <div className="moves">
+            <p>list of moves</p>
+            <div className="moves-flex">
+              {poke.moves.map((move, index) => (
+                <p key={index} className="move">
+                  {move.move.name}
+                </p>
+              ))}
+            </div>
+          </div>
         </div>
-        // <div>
-        //     {pokemon.map((poke, index) => (
-        //           <p>{poke.weight}</p>
-        //         ))}
-        // </div>
-     )
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default Pokemon;
